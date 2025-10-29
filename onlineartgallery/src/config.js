9faf5dba140841ef4476d39884ccd83de6cebc29
Laxman <2300090230@kluniversity.in>
2025-10-29 11:17:53 +0530

    v2.0

--- onlineartgallery/src/config.js
+++ onlineartgallery/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     //url: "http://localhost:2030/apionlineart"
     //url: "http://localhost:3025"
-    url: "http://localhost:2080"
+    url: "http://ec2-3-19-246-14.us-east-2.compute.amazonaws.com:2080"
 }
 
 export default config;
